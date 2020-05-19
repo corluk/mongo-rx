678c2ceb87e771db25dea349e03320a40cb8a49c
osmancorluk <corluk@hotmail.com>
2020-05-20 00:48:06 +0300

    + mongoOperator to index.ts for export

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
+import { mongoOperator } from './src/MongoOperators';
  
 import {MongoRx} from "./src/MongoRx"
 
 import {mongoUriBuilder, MongoUriBuilderConfig} from "./src/MongUriBuilder"
 export default MongoRx 
 
-export {mongoUriBuilder,MongoUriBuilderConfig,MongoRx}
+export {mongoUriBuilder,MongoUriBuilderConfig,MongoRx,mongoOperator}
 
