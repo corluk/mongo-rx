4032f7758938c23532056e373d22f17c8fcd4489
osmancorluk <corluk@hotmail.com>
2020-06-01 15:46:21 +0300

    added mongoRxCsv for export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { MongoRxCSV } from './src/MongoRxCsv';
 import { MongoRxCollection } from './src/MongoRxCollection';
  
  
@@ -6,5 +7,5 @@ import {MongoRx} from "./src/MongoRx"
 import {mongoUriBuilder, MongoUriBuilderConfig} from "./src/MongUriBuilder"
 export default MongoRx 
 
-export {mongoUriBuilder,MongoUriBuilderConfig,MongoRx , MongoRxCollection}
+export {mongoUriBuilder,MongoUriBuilderConfig,MongoRx , MongoRxCollection , MongoRxCSV}
 
